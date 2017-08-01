72ac0987ed03dfafe4b0f0e58e4dec400c075de9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2017-08-01 15:47:07 +0700

    assets precompilation

--- app/assets/javascripts/applicationscript.js
+++ app/assets/javascripts/applicationscript.js
@@ -2,11 +2,13 @@ var ready = function(){
 	$('html, body').animate({scrollTop:0}, 'slow');
 	position = $('#portfolio').offset();
 	top_position = parseInt(position["top"]);
-	$('#portfoliobutton').click(function(e){
+	$('.portfoliobutton').click(function(e){
 		e.preventDefault();
 		/*window.scrollTo(0, top_position);*/
 		$('html, body').animate({scrollTop: top_position}, 'slow');
 	});
+
+
 
 }
 
