2ebe66708a65edc766753ea326d697df0384572e
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2018-12-04 14:22:46 +0100

    responsive links to ext sites

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
-	layout 'application'
-	def index
-	end
-	def blog
-	end
-	def contact
-	end
+  # layout 'application'
+  def index; end
+  def blog; end
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-	resources :pages
-	get '/blog', to: 'pages#blog', as: 'blog'
-	get '/contact', to: 'pages#contact', as: 'contact'
-	root "pages#index"
-
+  resources :pages
+  get '/blog', to: 'pages#blog', as: 'blog'
+  get '/contact', to: 'pages#contact', as: 'contact'
+  root "pages#index"
 end
