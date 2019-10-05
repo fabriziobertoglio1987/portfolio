2eccd9723407f25d2ad483dee2474f1e91af9719
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2019-10-05 13:06:05 +0800

    env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Configure CloudFront
-  config.action_controller.asset_host = ENV.fetch('PORTFOLIO_AWS_CLOUD_FRONT_URL')
-  config.action_mailer.asset_host = ENV.fetch('PORTFOLIO_AWS_CLOUD_FRONT_URL')
+  config.action_controller.asset_host = ENV['PORTFOLIO_AWS_CLOUD_FRONT_URL']
+  config.action_mailer.asset_host = ENV['PORTFOLIO_AWS_CLOUD_FRONT_URL']
 
   config.public_file_server.enabled = true
   config.assets.compile = true
